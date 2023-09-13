dc7c568833150a5b4a931b49f2b92d3202574cc1
MagnusRichert <90255355+MagnusRichert@users.noreply.github.com>
2023-09-13 13:37:21 +0200

    renaming variables, mesh changes

--- Skript.py
+++ Skript.py
@@ -27,25 +27,25 @@ def generate_gcode(data, pattern):
 
     #add beginning gcode
     gcode.writelines("G21\n")   #set to mm
-    gcode.writelines("G28\n")
-    gcode.writelines("G0 "+start_coordinates+"\n")
-    gcode.writelines("G92 X0 Y0 Z0\n") # set new home to..
-    gcode.writelines("G0 F300\n\n")  #set speed to 300 units/min i guess
+    gcode.writelines("G28\n")   #home printhead
+    gcode.writelines(f"G0 X{offset_x:.2f} Y{offset_y:.2f} Z{offset_z:.2f}\n")   #move to offset location
+    gcode.writelines("G92 X0 Y0 Z0\n") #set current position as home
+    gcode.writelines(f"G0 F{speed:.0f}\n\n")  #set speed to XXX units/min i guess
 
     #iterate through short side of well
-    for width in range (int(data['number_short'])):
+    for number_y in range (int(data['number_y'])):
         # iterate through long side of well
-        for length in range (int(data['number_long'])):
+        for number_x in range (int(data['number_x'])):
 
             #check if well number is on list for not scratching
-            well_number = width * data['number_long'] + length + 1
+            well_number = number_y * data['number_y'] + number_x + 1
             if well_number in []:
                 print(f"Skipped well {well_number:.0f}")
                 continue
 
             #calculate center points for well
-            center_x = data['distance_short'] + length * (data['diameter'] + data['distance_well']) + data['diameter'] / 2
-            center_y = data['distance_long'] + width * (data['diameter'] + data['distance_well']) + data['diameter'] / 2
+            center_x = data['distance_x'] + number_x * (data['diameter'] + data['distance_well']) + data['diameter'] / 2
+            center_y = data['distance_y'] + number_y * (data['diameter'] + data['distance_well']) + data['diameter'] / 2
             depth = data['depth']
 
             #move above center of well and into it
@@ -54,20 +54,15 @@ def generate_gcode(data, pattern):
             gcode.writelines(f"G0 Z{-depth/2:.2f}\n")
 
             #adds gcode according to pattern
-            if pattern == "line":
-                start_x, end_x = line_through_x(center_x, data['diameter'], data['tip_offset'])
-                gcode.writelines(f"G0 X{start_x:.2f}\n")
-                gcode.writelines(f"G0 Z{-depth:.2f}\n")
-                gcode.writelines(f"G0 X{end_x:.2f}\n")
-                gcode.writelines(f"G0 Z{-depth/2:.2f}\n")
+            if pattern == "mesh":
+                if mesh_line_distance*mesh_line_number >= data['diameter']: #check if mesh is possible
+                    print("The mesh is not possible with current settings of line number and distance!")
+                    exit(1)
 
-            elif pattern == "mesh":
-                #side_offset = input("Specify a side offset. The outermost lines will wil") #for later maybe
-                line_distance = data['diameter']/(mesh_line_number+1)
-                for ii in range(mesh_line_number):
-                    y_postition = center_y - data['diameter']/2 + line_distance * (1+ii)
-                    start_x, end_x = line_through_x(center_x, data['diameter'], data['tip_offset'], y_offset=center_y-y_postition)
-                    gcode.writelines(f"G0 X{start_x:.2f} Y{y_postition:.2f}\n")
+                y_cordinates = [center_y+(i-mesh_line_number/2)*mesh_line_distance+mesh_line_distance/2 for i in range(mesh_line_number)]
+                for y_position in y_cordinates:
+                    start_x, end_x = line_through_x(center_x, data['diameter'], offset_tip, y_offset=center_y-y_position)
+                    gcode.writelines(f"G0 X{start_x:.2f} Y{y_position:.2f}\n")
                     gcode.writelines(f"G0 Z{-depth:.2f}\n")
                     gcode.writelines(f"G0 X{end_x:.2f}\n")
                     gcode.writelines(f"G0 Z{-depth/2:.2f}\n")
@@ -84,24 +79,36 @@ def generate_gcode(data, pattern):
 
     #return to start and close
     gcode.writelines("G0 X0 Y0 Z10\n")
-    gcode.writelines("M00 \"Please remove the tip to end print :)\\n"")
+    gcode.writelines("M00 \"Please remove the tip to end print :)\"\n")
     gcode.writelines("M30\n")
     gcode.close()
     print(f"Succesfully generated {gcode_name}!")
 
 ###TEST VARIABLES / remove later
-circle_radius = 6
-mesh_line_number = 8
-start_coordinates = "X50 Y50 Z30"
+file_path = "24well.txt"
+circle_radius = 6   #radius in mm
+mesh_line_number = 6    #number of lines in mesh pattern
+mesh_line_distance = 1.2  #distance between mesh lines
+offset_tip = 0.5
+offset_x = 100
+offset_y = 40
+offset_z = 50
+pattern = "mesh"
+speed = 3000
 
 
 ###MAIN SKRIPT
 if __name__ == '__main__':
 
-    file_path = input('Please enter the filename of your well data and press Enter: ')
+    if input("Use predefined values yes/no:") == "no":
+        file_path = input('Please enter the filename of your well data and press Enter: ')
+        offset_tip = float(input("Enter tip offset:"))
+        offset_x = float(input("Enter x offset:"))
+        offset_y = float(input("Enter y offset:"))
+        offset_z = float(input("Enter z offset:"))
+        mesh_line_number = int(input("Enter number of lines:"))
+        mesh_line_distance = float(input("Enter distance between lines:"))
     data = load_well_data(file_path)
-    print(data)
-    pattern = input('Please specify the pattern you want: line, mesh: ')
     print('Starting G-Code generation...')
     generate_gcode(data, pattern)
 
